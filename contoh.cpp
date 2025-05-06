f5d835ac0da3da1252d1bd531e1f6582892e4566
fadilsyy <fadilasytar@gmail.com>
2025-05-06 18:39:10 +0700

    contoh

--- contoh.cpp
+++ contoh.cpp
@@ -38,3 +38,16 @@ public:
         return 3.14 * r * r;
     }
 };
+
+int main()
+{
+    PersegiPanjang objekPP; //membuat objek
+    Lingkaran ol;
+
+    cout << "Masukkan panjang : ";
+    cin >> objekPP.panjang;
+    cout << "Masukkan lebar : ";
+    cin >> objekPP.lebar;
+    cout << "Luas Persegi Panjang : " << 
+    objekPP.luasPp(objekPP.panjang, objekPP.lebar) << endl; 
+}
